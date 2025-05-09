ad73157228446a2a94fcb0aca6ed213995938d41
Anna Ogiiko <listopadiya@gmail.com>
2025-05-10 01:10:41 +0300

    Update and rename constants.py to v2_config.py

--- quartz_solar_forecast/forecasts/constants.py
+++
@@ -1,3 +0,0 @@
-# download xgb model from google drive
-MODEL_FILE = "model_10_202405.ubj"
-FILE_ID = "1O34gyQ67rvrP9VFkNaagTDM9IP4iqAjM"
---
+++ quartz_solar_forecast/forecasts/v2_config.py
@@ -0,0 +1,4 @@
+# download xgb model from huggingface
+REPO_ID = "openclimatefix/open-source-quartz-solar-forecast"
+FILE_PATH = "models/v2/model_10_202405.ubj.zip"
+MODEL_NAME = "model_10_202405.ubj"
