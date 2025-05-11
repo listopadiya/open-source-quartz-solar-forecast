90d48885fc6d88153f6761f17cf6f6b5d0cd5bdc
Anna Ogiiko <listopadiya@gmail.com>
2025-05-11 22:38:25 +0300

    Update test_forecast.py

--- tests/unit/test_forecast.py
+++ tests/unit/test_forecast.py
@@ -1,5 +1,7 @@
 from quartz_solar_forecast.forecast import predict_ocf, predict_tryolabs, run_forecast
 from quartz_solar_forecast.pydantic_models import PVSite
+from quartz_solar_forecast.tests.mocks import mock_weather_api
+
 from datetime import datetime, timedelta
 
 import numpy as np
@@ -11,27 +13,8 @@ import pytest
 def site():
     return PVSite(latitude=51.75, longitude=-1.25, capacity_kwp=1.25)
 
-# Fixture for getting hourly data from open-meteo
-@pytest.fixture
-def dummy_weatherservice(monkeypatch):
-    # Monkeypatch get_hourly_weather method:
-    # behavior same to original method, returns dummy weather data
-    def mock_get_hourly_weather(self, latitude, longitude, start_date, end_date, variables):
-        mock_hourly_date = pd.date_range(
-        	start = pd.to_datetime(start_date, format="%Y-%m-%d", utc = False),
-        	end = pd.to_datetime(end_date, format="%Y-%m-%d", utc = False) + pd.Timedelta(days=1),
-        	freq = pd.Timedelta(hours=1),
-        	inclusive = "left"
-        )
-        mock_weather_df = pd.DataFrame({ "date": mock_hourly_date })
-        # Fill with zeroes (fake weather data)
-        for v in variables:
-            mock_weather_df[v] = 0.0
-        return mock_weather_df
 
-    monkeypatch.setattr(WeatherService, "get_hourly_weather", mock_get_hourly_weather)
-
-def test_run_forecast(site, dummy_weatherservice):
+def test_run_forecast(site, mock_weather_api):
     ts = datetime.today() - timedelta(weeks=2)
 
     # run model with icon, gfs and ukmo nwp
